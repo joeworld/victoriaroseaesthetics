4394bdbc3c7618469757777f0550d944dc82df39
Marco Antonio <54124842+marcoantonio123456@users.noreply.github.com>
2021-04-01 18:58:46 -0400

    .

--- resources/views/procedures/womens-health.blade.php
+++ resources/views/procedures/womens-health.blade.php
@@ -5,12 +5,172 @@
 @section('content')
 
     <div class="section padding-top-9em">
+        <div class="section">
+            <div class="container">
+                <div class="heading-wrapper-centered">
+                    <h1>GYNECOLOGY TREATMENT</h1>
+                    <p class="medium-width centered">Gynecology treatments: stress and urge incontinence, vaginal tightening and rejuvenation.</p>
+                    <p>PRP “O-SHOT”</p>
+                    <p class="medium-width centered">PRP therapy helps to promote orgasm in women.</p>
+
+                </div>
+            </div>
+            <div class="container no-padding-mobile">
+                <div class="inner-spacing">
+                    <div data-duration-in="300" data-duration-out="100" class="faq-wrapper w-tabs">
+                        <div class="faq-tabs-menu w-tab-menu" role="tablist">
+                            <a data-w-tab="Tab 1" class="faq-tab-link w-inline-block w-tab-link w--current" id="w-tabs-0-data-w-tab-0" href="#w-tabs-0-data-w-pane-0" role="tab" aria-controls="w-tabs-0-data-w-pane-0" aria-selected="true">
+                                <div>Procedures</div>
+                            </a>
+                            <a data-w-tab="Tab 2" class="faq-tab-link w-inline-block w-tab-link" tabindex="-1" id="w-tabs-0-data-w-tab-1" href="#w-tabs-0-data-w-pane-1" role="tab" aria-controls="w-tabs-0-data-w-pane-1" aria-selected="false">
+                                <div>Benefits</div>
+                            </a>
+                        </div>
+                        <div class="faq-tabs-content w-tab-content">
+                            <div data-w-tab="Tab 1" class="w-tab-pane w--tab-active" id="w-tabs-0-data-w-pane-0" role="tabpanel" aria-labelledby="w-tabs-0-data-w-tab-0">
+                                <div class="faq-content">
+                                    <div class="faq-title">
+                                        <h4>Can you really rejuvenate your sex life with the O-Shot® procedure?</h4>
+                                        <img src="https://assets.website-files.com/5f8d4e0da2afa35b4a26d78e/5fabd83a6bfe49516e2d1161_menu_close_icon.svg" loading="lazy" alt="" class="faq-icon mobile-only" />
+                                    </div>
+                                    <div class="faq-answer">
+                                        <p>
+                                            The Orgasm “O” Shot, is a non-surgical treatment which pledges to increase sexual arousal and rejuvenate the vagina. Patients report stronger and more frequent orgasms, increased natural lubrication and greater arousal after having the procedure.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="faq-content">
+                                    <div class="faq-title">
+                                        <h4>Is the O-Shot a drug?</h4>
+                                        <img src="https://assets.website-files.com/5f8d4e0da2afa35b4a26d78e/5fabd83a6bfe49516e2d1161_menu_close_icon.svg" loading="lazy" alt="" class="faq-icon mobile-only" />
+                                    </div>
+                                    <div class="faq-answer">
+                                        <p>
+                                            The O-Shot is not a drug. It’s a procedure performed in a medical office in which your own blood platelets are injected into vaginal tissue. The way it works is that platelets naturally attract your own stem cells to the injected area, and “generate healthier and more functional tissue in the areas of sexual response in the vagina (G-Spot, O-Spot, Skene’s Glands, urethra, and vaginal wall).”
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="faq-content">
+                                    <div class="faq-title">
+                                        <h4>How does the O-Shot cause vaginal rejuvenation?</h4>
+                                        <img src="https://assets.website-files.com/5f8d4e0da2afa35b4a26d78e/5fabd83a6bfe49516e2d1161_menu_close_icon.svg" loading="lazy" alt="" class="faq-icon mobile-only" />
+                                    </div>
+                                    <div class="faq-answer">
+                                        <p>
+                                            The shot works by using “high quality” Platelet-Rich Plasma (PRP) produced from the patient’s own blood. The PRP contains cell regenerating growth factors which, when injected into specific areas of the vagina, trigger stem cells to increase blood flow and generate healthy tissue growth as well as help improve the vascularization of the area.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="faq-content">
+                                    <div class="faq-title">
+                                        <h4>That sounds complicated… what is the procedure like?</h4>
+                                        <img src="https://assets.website-files.com/5f8d4e0da2afa35b4a26d78e/5fabd83a6bfe49516e2d1161_menu_close_icon.svg" loading="lazy" alt="" class="faq-icon mobile-only" />
+                                    </div>
+                                    <div class="faq-answer">
+                                        <p>
+                                            The O-Shot procedure begins with a simple blood draw from the arm. This sample is then put into a special centrifuge to produce the Platelet-Rich Plasma. Once the PRP is extracted from the blood, it is injected into the numbed area near the clitoris and upper vagina.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="faq-content">
+                                    <div class="faq-title">
+                                        <h4>Are there O-Shot Complications?</h4>
+                                        <img src="https://assets.website-files.com/5f8d4e0da2afa35b4a26d78e/5fabd83a6bfe49516e2d1161_menu_close_icon.svg" loading="lazy" alt="" class="faq-icon mobile-only" />
+                                    </div>
+                                    <div class="faq-answer">
+                                        <p>
+                                            Most patients experience minimal to no side effects after receiving the orgasm shot. However, you may experience mild redness, swelling and/or numbness for the first few days to one week.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="faq-content">
+                                    <div class="faq-title">
+                                        <h4>Do I need more than one O-Shot?</h4>
+                                        <img src="https://assets.website-files.com/5f8d4e0da2afa35b4a26d78e/5fabd83a6bfe49516e2d1161_menu_close_icon.svg" loading="lazy" alt="" class="faq-icon mobile-only" />
+                                    </div>
+                                    <div class="faq-answer">
+                                        <p>
+                                            Patients can just have one orgasm shot or come back for more, which will build on the existing results. 8 weeks is generally recommended between treatments to ensure the full effects of the first treatment have been felt. However, you can speak with a medical provider about the timeframe that works best for you.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div data-w-tab="Tab 2" class="w-tab-pane" id="w-tabs-0-data-w-pane-1" role="tabpanel" aria-labelledby="w-tabs-0-data-w-tab-1">
+                                <div class="faq-content">
+                                    <div class="faq-title">
+                                        <h4>What results can I expect?</h4>
+                                        <img src="https://assets.website-files.com/5f8d4e0da2afa35b4a26d78e/5fabd83a6bfe49516e2d1161_menu_close_icon.svg" loading="lazy" alt="" class="faq-icon mobile-only" />
+                                    </div>
+                                    <div class="faq-answer">
+                                        <p>
+                                            The results can vary depending on the patient’s starting point. For example, factors such as age, medical conditions, and hormonal problems can determine the results. The injection is not a “magic stick” – 50 percent of women may experience instantaneous “wow effects” while the other half may just notice a marked improvement.
+                                        </p>
+                                        <p>
+                                            Patients have reported a range of positive effects from the shots including:
+                                        </p>
+                                        <ul>
+                                            <li>
+                                                Increased sensation.
+                                            </li>
+                                            <li>
+                                                Greater arousal from clitoral stimulation.
+                                            </li>
+                                            <li>
+                                                The ability to have an orgasm from penetrative sex when previously unable.
+                                            </li>
+                                            <li>
+                                                Younger and smoother skin of the vulva.
+                                            </li>
+                                            <li>
+                                                Stronger and more frequent orgasms.
+                                            </li>
+                                            <li>
+                                                Improvement in urinary incontinence.
+                                            </li>
+                                            <li>
+                                                Decreased pain for those experiencing painful sex.
+                                            </li>
+                                            <li>
+                                                Increased natural lubrication.
+                                            </li>
+                                            <li>
+                                                Increased ability to have a vaginal orgasm.
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="faq-content">
+                                    <div class="faq-title">
+                                        <h4>How long does it take before I see results from an O-Shot?</h4>
+                                        <img src="https://assets.website-files.com/5f8d4e0da2afa35b4a26d78e/5fabd83a6bfe49516e2d1161_menu_close_icon.svg" loading="lazy" alt="" class="faq-icon mobile-only" />
+                                    </div>
+                                    <div class="faq-answer">
+                                        <p>You may experience an increase in sexual desire and pleasure immediately, but full results are usually enjoyed after 3 months. Generally, women will begin to experience heightened sensation in the first 3-7 days and new tissue will begin to develop over the next 3-9 weeks. This will peak around the 3-month mark.</p>
+
+                                    </div>
+                                </div>
+                                <div class="faq-content">
+                                    <div class="faq-title">
+                                        <h4>How long does the O Shot last?</h4>
+                                        <img src="https://assets.website-files.com/5f8d4e0da2afa35b4a26d78e/5fabd83a6bfe49516e2d1161_menu_close_icon.svg" loading="lazy" alt="" class="faq-icon mobile-only" />
+                                    </div>
+                                    <div class="faq-answer">
+                                        <p>O-Shot procedure results can vary depending on the patient, but they generally last between 14 and 18 months. On average, women repeat the procedure every 18 months</p>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="w-container">
-            <div class="contact-info-wrapper">
+            {{-- <div class="contact-info-wrapper">
                 <div id="w-node-b2551c3a-8701-fea8-c6df-cdb1726c253b-d48e3962" class="contact-info">
                     <h4>@yield('title')</h4>
                 </div>
-            </div>
+            </div> --}}
             {{-- <div class="subnavigation">
                 <div class="subnavigation-wrapper">
                     <a href="/services/brows-lashes-all" class="subnavigation-link w--current">All</a>
@@ -26,21 +186,60 @@
                     </div>
                 </div>
             </div> --}}
+            <div class="section padding-3-5em bcg2">
+                <div class="container">
+                    <div class="newsletter-wrapper no-padding-tb">
+                        <div class="newsletter-content">
+                            <h3 class="light">Get our weekly newsletter</h3>
+                            <p class="small-width light">Sign up for our newsletter. We will promise to send you emails relevant to you, once per week only.</p>
+                            <div class="newsletter-form-wrapper">
+                                <div class="newsletter-form-block w-form">
+                                    <form id="email-form" name="email-form" data-name="Email Form" class="newsletter-form">
+                                        <input type="email" class="text-field light w-input" maxlength="256" name="Email-2" data-name="Email 2" placeholder="Your Email" id="Email-2" required="" />
+                                        <div class="button-wrapper">
+                                            <div class="button-bcg"></div>
+                                            <input type="submit" value="Sign me up" data-wait="Please wait..." class="form-button light w-button" />
+                                        </div>
+                                    </form>
+                                    <div class="w-form-done"><div>Thank you! Your submission has been received!</div></div>
+                                    <div class="w-form-fail"><div>Oops! Something went wrong while submitting the form.</div></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="newsletter-image">
+                            <img
+                                src="https://assets.website-files.com/5f8d4e0da2afa35b4a26d78e/5fa67aa4f4753103830ae13d_newsletter_img.jpg"
+                                loading="lazy"
+                                width="434"
+                                sizes="(max-width: 479px) 93vw, (max-width: 767px) 28vw, (max-width: 1439px) 32vw, 100vw"
+                                srcset="
+                                    https://assets.website-files.com/5f8d4e0da2afa35b4a26d78e/5fa67aa4f4753103830ae13d_newsletter_img-p-500.jpeg 500w,
+                                    https://assets.website-files.com/5f8d4e0da2afa35b4a26d78e/5fa67aa4f4753103830ae13d_newsletter_img-p-800.jpeg 800w,
+                                    https://assets.website-files.com/5f8d4e0da2afa35b4a26d78e/5fa67aa4f4753103830ae13d_newsletter_img.jpg        868w
+                                "
+                                alt=""
+                                class="cover-image"
+                            />
+                            <img src="https://assets.website-files.com/5f8d4e0da2afa35b4a26d78e/5fa67aee3a458f6c9c84dbb3_circle_pattern.png" loading="lazy" width="91" alt="" class="newsletter-pattern" />
+                        </div>
+                    </div>
+                </div>
+            </div>
 
 
 
             {{-- Add images and sub img --}}
             <div>
                 <div class="padding-top-9em">
-                    <div class="big-title">BROW & LASH TINTING</div>
+                    <div class="big-title">GYNECOLOGY TREATMENT</div>
                     <br>
-                    <p>An eyelash tint consists of adding a special dye to your eyelashes, giving you weeks of dark, full lashes without the assistance of mascara. You recline in a salon chair and a technician applies the dye to your lashes while you keep your eyes closed for about 10 minutes. It's a quick, relatively painless process.</p>
-                    <p>Eyebrow tinting is the process of applying semi-permanent dye to enhance, shape, and define your brows.</p>
+                    <p>Gynecology treatments: stress and urge incontinence, vaginal tightening and rejuvenation</p>
                 </div>
-                <div class="padding-top-9em">
-                    <div class="big-title">BROW LAMINATION</div>
+                <div>
                     <br>
-                    <p>Brow lamination is a two-step eyebrow perming treatment that involves correcting the brow shape by setting the hairs in place using a chemical solution. In the first step, a cream is applied that breaks down bonds in the eyebrow hairs so they can be manipulated into a new position.</p>
+                    <div class="product-title">PRP “O-SHOT”</div>
+                    <br>
+                    <p>PRP therapy helps to promote orgasm in women.</p>
                 </div>
                 <div class="padding-top-9em">
                     <div class="big-title">LASH LIFT</div>
