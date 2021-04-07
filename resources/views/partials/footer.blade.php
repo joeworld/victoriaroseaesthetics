361358295e56ee3705b4aeded7370e7f43151d4e
Marco Antonio <54124842+marcoantonio123456@users.noreply.github.com>
2021-04-07 19:27:34 -0400

    .

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -17,26 +17,11 @@
                     </div>
                     <div class="footer-menu">
                         <div class="footer-menu-title light">Hours</div>
-                        <a class="footer-link light">Tuesday:
-                                10:00am
-
-                                - 6:00pm</a>
-                        <a class="footer-link light">Wednesday:
-                                10:00am
-
-                                - 6:00pm</a>
-                        <a class="footer-link light">Thursday:
-                                12:00pm
-
-                                - 8:00pm</a>
-                        <a class="footer-link light">Friday:
-                                10:00pm
-
-                                - 6:00pm</a>
-                        <a class="footer-link light">Saturday:
-                                10:00am
-
-                                - 3:00pm</a>
+                        <a class="footer-link light hours-op">Tuesday: 10:00am - 6:00pm</a>
+                        <a class="footer-link light hours-op">Wednesday: 10:00am - 6:00pm</a>
+                        <a class="footer-link light hours-op">Thursday: 12:00pm - 8:00pm</a>
+                        <a class="footer-link light hours-op">Friday: 10:00pm - 6:00pm</a>
+                        <a class="footer-link light hours-op">Saturday: 10:00am - 3:00pm</a>
 
                     </div>
                 </div>
--- resources/views/procedures/skin-resurfacing.blade.php
+++ resources/views/procedures/skin-resurfacing.blade.php
@@ -3,40 +3,44 @@
 @section('page', '5fa686c7b73fbb4dd28a52dd')
 
 @section('content')
-
-    <div class="section padding-top-9em">
-        <div class="section">
-            <div class="container">
-                <div class="heading-wrapper-centered">
-                    <div class="contact-info-wrapper">
-                        <div id="w-node-b2551c3a-8701-fea8-c6df-cdb1726c253b-d48e3962" class="contact-info">
-                            <h4>@yield('title')</h4>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="padding-top-6em">
-                            <p>
-                                The TwinLight® Fractional Rejuvenation is the latest development in laser technology is scientifically proven to optimize the smoothness of your skin and give it a fresher, healthier look.
-                            </p>
-                            <p>
-                                Skin resurfacing and rejuvenation treatments can be performed either in ablative modes with an Er: YAG laser or non-ablatively with an Nd: YAG laser, depending on the particular aesthetic outcome that the patient and practitioner wish to achieve. Ablative and non-ablative treatments can also be combined into a comprehensive anti-aging treatment, such as Fotona’s TwinLight® Fractional Rejuvenation system.
-                                Past skin rejuvenation procedures treated large areas of skin, whether aged or not. Fotona’s revolutionary TwinLight® Fractional Rejuvenation process, the laser light precisely targets imperfections in the skin, leaving the surrounding healthy skin untouched. The heat from the laser energy also stimulates the production of new collagen in the dermis, and the surrounding, untreated skin helps to speed the rejuvenation process.
-                            </p>
-                            <p>
-                                Treatments can extend from light touch-ups to deep collagen remodeling, ideal for patients who want drastic rejuvenation effects, but are not inclined towards aggressive surgical or chemical alternatives. This treatment is suitable for all skin types and tones, with minimal patient downtime.
-                            </p>
-                            <div class="team-slide-image-wrapper-2 padding-top-6em">
-                                <figure style="max-width: 1200px;" class="w-richtext-align-fullwidth w-richtext-figure-type-image">
-                                    <iframe width="100%" height="500" src="https://www.youtube.com/embed/r_p5MhU7b84" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                                </figure>
-                            </div>
-                        </div>
+    <div class="section no-bottom-padding">
+        <div class="container">
+            <div class="heading-wrapper-centered">
+                <div class="contact-info-wrapper">
+                    <div id="w-node-b2551c3a-8701-fea8-c6df-cdb1726c253b-d48e3962" class="contact-info">
+                        <h4>@yield('title')</h4>
                     </div>
                 </div>
             </div>
         </div>
-        @include('partials.book-btn')
     </div>
+
+    <div class="section">
+        <div class="container">
+            <div class="w-layout-grid grid-2-column">
+                <div id="w-node-c6f28ec5-e2b7-2034-7b7a-104e024c9d1b-b826d78f" class="grid-content">
+                    <p>
+                        The TwinLight® Fractional Rejuvenation is the latest development in laser technology is scientifically proven to optimize the smoothness of your skin and give it a fresher, healthier look.
+                    </p>
+                    <p>
+                        Skin resurfacing and rejuvenation treatments can be performed either in ablative modes with an Er: YAG laser or non-ablatively with an Nd: YAG laser, depending on the particular aesthetic outcome that the patient and practitioner wish to achieve. Ablative and non-ablative treatments can also be combined into a comprehensive anti-aging treatment, such as Fotona’s TwinLight® Fractional Rejuvenation system.
+                        Past skin rejuvenation procedures treated large areas of skin, whether aged or not. Fotona’s revolutionary TwinLight® Fractional Rejuvenation process, the laser light precisely targets imperfections in the skin, leaving the surrounding healthy skin untouched. The heat from the laser energy also stimulates the production of new collagen in the dermis, and the surrounding, untreated skin helps to speed the rejuvenation process.
+                    </p>
+                    <p>
+                        Treatments can extend from light touch-ups to deep collagen remodeling, ideal for patients who want drastic rejuvenation effects, but are not inclined towards aggressive surgical or chemical alternatives. This treatment is suitable for all skin types and tones, with minimal patient downtime.
+                    </p>
+                    @include('partials.book-btn')
+                </div>
+                <div id="w-node-_8449cbd9-7126-ef1b-e7ee-a76cd55e5dd1-b826d78f" class="grid-image-block-2 padding-left">
+                    <figure style="max-width: 1200px;" class="w-richtext-align-fullwidth w-richtext-figure-type-image">
+                        <iframe width="100%" height="500" src="https://www.youtube.com/embed/r_p5MhU7b84" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </figure>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
     {{-- services --}}
     @include('partials.services')
 
