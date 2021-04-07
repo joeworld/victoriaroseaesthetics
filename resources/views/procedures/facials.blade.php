e59c6fa53440395551c83c72abc9344bd7b7fcf4
Marco Antonio <54124842+marcoantonio123456@users.noreply.github.com>
2021-04-07 17:19:37 -0400

    .

--- resources/views/procedures/facials.blade.php
+++ resources/views/procedures/facials.blade.php
@@ -32,6 +32,22 @@
                         <div class="team-slide w-slide" role="group" style="transform: translateX(0px); opacity: 1;">
                             <div class="w-layout-grid grid-2-column align-center">
                                 <div class="team-slide-image-wrapper-2">
+                                    <img src="/img/facials-5.png" alt="">
+                                </div>
+                                <div class="team-slide-content-wrapper">
+                                    <div class="small-all-caps">PRP MICRONEEDLING</div>
+                                    <p>Microneedling with plasma-rich platelets (PRP) is a cosmetic treatment that stimulates collagen production by rolling fine needles over the skin and applying platelets, which are one of the components of blood.</p>
+                                    <p>The Beauty Booster Delivery Device offers precise depth and accurate dosing for a quick and effective treatment with minimal pain and downtime for patients. The 9 points multi-needle tip and vacuum tip reduces discomfort. The treatment can be completely customized with a wide range of applications to target dehydrated skin, fine lines, and wrinkles. </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div data-animation="outin" data-duration="500" data-infinite="1" class="team-slider w-slider" role="region" aria-label="carousel">
+                    <div class="w-slider-mask" id="w-slider-mask-0">
+                        <div class="team-slide w-slide" role="group" style="transform: translateX(0px); opacity: 1;">
+                            <div class="w-layout-grid grid-2-column align-center">
+                                <div class="team-slide-image-wrapper-2">
                                     <img src="/img/facials-2.png" alt="">
                                 </div>
                                 <div class="team-slide-content-wrapper">
@@ -79,12 +95,11 @@
                         <div class="team-slide w-slide" role="group" style="transform: translateX(0px); opacity: 1;">
                             <div class="w-layout-grid grid-2-column align-center">
                                 <div class="team-slide-image-wrapper-2">
-                                    <img src="/img/facials-5.png" alt="">
+                                    <img src="/img/facials-6.png" alt="">
                                 </div>
                                 <div class="team-slide-content-wrapper">
-                                    <div class="small-all-caps">PRP MICRONEEDLING</div>
-                                    <p>Microneedling with plasma-rich platelets (PRP) is a cosmetic treatment that stimulates collagen production by rolling fine needles over the skin and applying platelets, which are one of the components of blood.</p>
-                                    <p>The Beauty Booster Delivery Device offers precise depth and accurate dosing for a quick and effective treatment with minimal pain and downtime for patients. The 9 points multi-needle tip and vacuum tip reduces discomfort. The treatment can be completely customized with a wide range of applications to target dehydrated skin, fine lines, and wrinkles. </p>
+                                    <div class="small-all-caps">Alumier Glow Peel</div>
+                                    <p>With a variety of custom peels to choose from, each with a unique complex of ingredients, your cosmetic professional can customize your program of treatments to target your specific skin concerns whether it be optimum skin health & aging, pigmentation, acne, rosacea and more.</p>
                                 </div>
                             </div>
                         </div>
